a807f5e5b34eabbc043984adea4f23518f694dfe
Nikita <nikita.janakarajan907@gmail.com>
2022-05-22 15:49:54 +0200

    import fix

--- survival_benchmark/python/modules/modules.py
+++ survival_benchmark/python/modules/modules.py
@@ -4,7 +4,7 @@ from skorch.net import NeuralNet
 from sksurv.linear_model.coxph import BreslowEstimator
 from torch import nn
 
-from survival_benchmark.survival_benchmark.python.utils import (
+from survival_benchmark.python.utils.utils import (
     flatten,
     inverse_transform_survival_target,
 )
@@ -66,9 +66,7 @@ class DeepSurv(nn.Module):
         p_dropout=0.0,
     ):
         super().__init__()
-        self.log_hazard = HazardRegression(
-            input_dimension, 1, hidden_layer_sizes, activation, p_dropout
-        )
+        self.log_hazard = HazardRegression(input_dimension, 1, hidden_layer_sizes, activation, p_dropout)
 
     def forward(self, x):
         return self.log_hazard(x)
@@ -121,9 +119,7 @@ class CoxPHNet(BaseSurvivalNeuralNet):
 
     def predict_survival_function(self, X):
         log_hazard_ratios = self.forward(X)
-        survival_function = self.breslow.get_survival_function(
-            log_hazard_ratios
-        )
+        survival_function = self.breslow.get_survival_function(log_hazard_ratios)
         return survival_function
 
     def predict(self, X):
@@ -137,9 +133,7 @@ class CheerlaEtAlNet(CoxPHNet):
         return criterion
 
     def forward(self, X, training=False, device="cpu"):
-        y_infer = list(self.forward_iter(X, training=training, device=device))[
-            0
-        ][0]
+        y_infer = list(self.forward_iter(X, training=training, device=device))[0][0]
         return y_infer
 
 
@@ -160,21 +154,12 @@ class GDPNet(BaseSurvivalNeuralNet):
             )
 
         weight_decay = torch.sum(
-            torch.stack(
-                [
-                    torch.square(torch.norm(i, p=2))
-                    for i in self.module_.log_hazard.hazard[1:].parameters()
-                ]
-            )
+            torch.stack([torch.square(torch.norm(i, p=2)) for i in self.module_.log_hazard.hazard[1:].parameters()])
         )
         print(weight_decay)
         print(group_lasso)
         print(criterion)
-        return (
-            criterion
-            + self.module_.lambdaq * weight_decay
-            + self.module_.alpha * group_lasso
-        )
+        return criterion + self.module_.lambdaq * weight_decay + self.module_.alpha * group_lasso
 
 
 # Adapted from: https://github.com/gevaertlab/MultimodalPrognosis/blob/master/modules.py
@@ -184,15 +169,9 @@ class Highway(nn.Module):
         super(Highway, self).__init__()
 
         self.num_layers = num_layers
-        self.nonlinear = nn.ModuleList(
-            [nn.Linear(size, size) for _ in range(num_layers)]
-        )
-        self.linear = nn.ModuleList(
-            [nn.Linear(size, size) for _ in range(num_layers)]
-        )
-        self.gate = nn.ModuleList(
-            [nn.Linear(size, size) for _ in range(num_layers)]
-        )
+        self.nonlinear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+        self.linear = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
+        self.gate = nn.ModuleList([nn.Linear(size, size) for _ in range(num_layers)])
         self.f = f
 
     def forward(self, x):
@@ -228,9 +207,7 @@ class cheerla_et_al_genomic_encoder(nn.Module):
 class cheerla_et_al_clinical_encoder(nn.Module):
     def __init__(self, input_dimension, encoding_dimension=512) -> None:
         super().__init__()
-        self.encode = nn.Sequential(
-            nn.Linear(input_dimension, encoding_dimension), nn.Sigmoid()
-        )
+        self.encode = nn.Sequential(nn.Linear(input_dimension, encoding_dimension), nn.Sigmoid())
 
     def forward(self, X):
         return self.encode(X)
@@ -246,18 +223,11 @@ class cheerla_et_al(nn.Module):
         p_multimodal_dropout=0.25,
     ) -> None:
         super().__init__()
-        block_encoders = [
-            cheerla_et_al_clinical_encoder(len(blocks[0]), encoding_dimension)
-        ] + [
-            cheerla_et_al_genomic_encoder(
-                len(i), encoding_dimension, p_dropout
-            )
-            for i in blocks[1:]
+        block_encoders = [cheerla_et_al_clinical_encoder(len(blocks[0]), encoding_dimension)] + [
+            cheerla_et_al_genomic_encoder(len(i), encoding_dimension, p_dropout) for i in blocks[1:]
         ]
         self.block_encoders = nn.ModuleList(block_encoders)
-        self.hazard = HazardRegression(
-            encoding_dimension, 1, [128], nn.Sigmoid, p_dropout
-        )
+        self.hazard = HazardRegression(encoding_dimension, 1, [128], nn.Sigmoid, p_dropout)
         self.blocks = blocks
         self.p_multimodal_dropout = p_multimodal_dropout
         self.M = M
@@ -265,28 +235,19 @@ class cheerla_et_al(nn.Module):
     def multimodal_dropout(self, X, p_dropout, blocks, upweight=True):
         for block in blocks:
             if not torch.all(X[:, block] == 0):
-                msk = torch.where(
-                    (torch.rand(X.shape[0]) <= p_dropout).long()
-                )[0]
-                X[:, torch.tensor(block)][msk, :] = torch.zeros(
-                    X[:, torch.tensor(block)][msk, :].shape
-                )
+                msk = torch.where((torch.rand(X.shape[0]) <= p_dropout).long())[0]
+                X[:, torch.tensor(block)][msk, :] = torch.zeros(X[:, torch.tensor(block)][msk, :].shape)
         if upweight:
             X = X / (1 - p_dropout)
         return X
 
     def forward(self, X):
         if self.p_multimodal_dropout > 0 and self.training:
-            X = self.multimodal_dropout(
-                X, self.p_multimodal_dropout, self.blocks
-            )
-        block_encoded = [
-            self.block_encoders[i](X[:, self.blocks[i]])
-            for i in range(len(self.blocks))
-        ]
+            X = self.multimodal_dropout(X, self.p_multimodal_dropout, self.blocks)
+        block_encoded = [self.block_encoders[i](X[:, self.blocks[i]]) for i in range(len(self.blocks))]
         # Only take non-missing and non-dropped modalities into account
-        joint = torch.sum(
-            torch.stack(block_encoded, axis=2), axis=2
-        ) / torch.sum(torch.stack(block_encoded, axis=2) != 0, axis=2)
+        joint = torch.sum(torch.stack(block_encoded, axis=2), axis=2) / torch.sum(
+            torch.stack(block_encoded, axis=2) != 0, axis=2
+        )
         hazard = self.hazard(joint)
         return hazard, block_encoded
