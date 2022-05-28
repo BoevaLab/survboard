f4bda3613e1437341926ce8d707e10ba4fd82563
Nikita <nikita.janakarajan907@gmail.com>
2022-05-28 20:06:49 +0200

    [FIX] outersplit range

--- survival_benchmark/python/scripts/driver.py
+++ survival_benchmark/python/scripts/driver.py
@@ -41,26 +41,16 @@ from survival_benchmark.python.utils.utils import (
 )
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "data_dir", type=str, help="Path to the folder containing data."
-)
+parser.add_argument("data_dir", type=str, help="Path to the folder containing data.")
 parser.add_argument(
     "config_path",
     type=str,
     help="Path to the parameters needed for training in JSON format.",
 )
-parser.add_argument(
-    "model_params", type=str, help="Path to model parameters json file"
-)
-parser.add_argument(
-    "results_path", type=str, help="Path where results should be saved"
-)
-parser.add_argument(
-    "model_name", type=str, help="Name of model being trained."
-)
-parser.add_argument(
-    "project", type=str, help="Cancer project from which data is being used"
-)
+parser.add_argument("model_params", type=str, help="Path to model parameters json file")
+parser.add_argument("results_path", type=str, help="Path where results should be saved")
+parser.add_argument("model_name", type=str, help="Name of model being trained.")
+parser.add_argument("project", type=str, help="Cancer project from which data is being used")
 parser.add_argument(
     "setting",
     type=str,
@@ -88,9 +78,7 @@ def main(
     # setup logging
     logging.basicConfig(
         handlers=[
-            logging.FileHandler(
-                os.path.join(results_path, f"{model_name}.log")
-            ),
+            logging.FileHandler(os.path.join(results_path, f"{model_name}.log")),
             logging.StreamHandler(sys.stdout),
         ],
     )
@@ -177,9 +165,7 @@ def main(
             )
             data_missing = data_missing.drop(columns=["OS", "OS_days"])
         else:
-            raise ValueError(
-                "`setting` must be in ['standard', 'missing', 'pancancer']"
-            )
+            raise ValueError("`setting` must be in ['standard', 'missing', 'pancancer']")
 
         data = data.drop(columns=["OS", "OS_days"])
         msk = (data != data.iloc[0]).any()
@@ -188,46 +174,24 @@ def main(
             data_missing = data_missing.loc[:, msk]
         if setting != "pancancer":
             # Train Test Splits
-            train_splits = pd.read_csv(
-                os.path.join(
-                    data_dir, f"splits/{project}/{cancer}_train_splits.csv"
-                )
-            )
-            test_splits = pd.read_csv(
-                os.path.join(
-                    data_dir, f"splits/{project}/{cancer}_test_splits.csv"
-                )
-            )
+            train_splits = pd.read_csv(os.path.join(data_dir, f"splits/{project}/{cancer}_train_splits.csv"))
+            test_splits = pd.read_csv(os.path.join(data_dir, f"splits/{project}/{cancer}_test_splits.csv"))
         else:
             train_splits = {}
             test_splits = {}
             for cancer in config["tcga_cancers"]:
                 print(cancer)
                 train_splits[cancer] = pd.read_csv(
-                    os.path.join(
-                        data_dir, f"splits/{project}/{cancer}_train_splits.csv"
-                    )
+                    os.path.join(data_dir, f"splits/{project}/{cancer}_train_splits.csv")
                 )
-                test_splits[cancer] = pd.read_csv(
-                    os.path.join(
-                        data_dir, f"splits/{project}/{cancer}_test_splits.csv"
-                    )
-                )
+                test_splits[cancer] = pd.read_csv(os.path.join(data_dir, f"splits/{project}/{cancer}_test_splits.csv"))
 
-        for outer_split in range(train_splits["BLCA"].shape[0]):
+        for outer_split in range(25):
             logger.info(f"Starting split: {outer_split + 1} / 25")
 
             if setting != "pancancer":
-                train_ix = (
-                    train_splits.iloc[outer_split, :]
-                    .dropna()
-                    .values.astype(int)
-                )
-                test_ix = (
-                    test_splits.iloc[outer_split, :]
-                    .dropna()
-                    .values.astype(int)
-                )
+                train_ix = train_splits.iloc[outer_split, :].dropna().values.astype(int)
+                test_ix = test_splits.iloc[outer_split, :].dropna().values.astype(int)
             else:
                 train_ix = []
                 test_ix = []
@@ -235,10 +199,7 @@ def main(
                     train_ix.append(
                         data.loc[data["clinical_cancer_type"] == cancer, :]
                         .iloc[
-                            train_splits[cancer]
-                            .iloc[outer_split, :]
-                            .dropna()
-                            .values,
+                            train_splits[cancer].iloc[outer_split, :].dropna().values,
                             :,
                         ]
                         .index.values.astype(int)
@@ -246,10 +207,7 @@ def main(
                     test_ix.append(
                         data.loc[data["clinical_cancer_type"] == cancer, :]
                         .iloc[
-                            test_splits[cancer]
-                            .iloc[outer_split, :]
-                            .dropna()
-                            .values,
+                            test_splits[cancer].iloc[outer_split, :].dropna().values,
                             :,
                         ]
                         .index.values.astype(int)
@@ -260,20 +218,13 @@ def main(
                 X_train = data.iloc[train_ix, :]
             else:
                 X_train = data.iloc[
-                    np.array(
-                        [item for sublist in train_ix for item in sublist]
-                    ),
+                    np.array([item for sublist in train_ix for item in sublist]),
                     :,
                 ]
                 X_test = {}
                 for cancer in config["tcga_cancers"]:
-                    X_test[cancer] = data.loc[
-                        data["clinical_cancer_type"] == cancer
-                    ].iloc[
-                        test_splits[cancer]
-                        .iloc[outer_split, :]
-                        .dropna()
-                        .values,
+                    X_test[cancer] = data.loc[data["clinical_cancer_type"] == cancer].iloc[
+                        test_splits[cancer].iloc[outer_split, :].dropna().values,
                         :,
                     ]
 
@@ -287,9 +238,7 @@ def main(
                     (
                         "categorical",
                         make_pipeline(
-                            OneHotEncoder(
-                                sparse=False, handle_unknown="ignore"
-                            ),
+                            OneHotEncoder(sparse=False, handle_unknown="ignore"),
                             StandardScaler(),
                         ),
                         np.where(X_train.dtypes == "object")[0],
@@ -306,33 +255,20 @@ def main(
                     pd.concat([event[train_ix], event_missing], axis=0),
                 )
             else:
-                y_train = transform_survival_target(
-                    time[train_ix], event[train_ix]
-                )
+                y_train = transform_survival_target(time[train_ix], event[train_ix])
 
             X_train = ct.fit_transform(X_train)
 
             X_train = pd.DataFrame(
                 X_train,
-                columns=data.columns[
-                    np.where(data.dtypes != "object")[0]
-                ].tolist()
-                + [
-                    f"clinical_{i}"
-                    for i in ct.transformers_[1][1][0]
-                    .get_feature_names()
-                    .tolist()
-                ],
+                columns=data.columns[np.where(data.dtypes != "object")[0]].tolist()
+                + [f"clinical_{i}" for i in ct.transformers_[1][1][0].get_feature_names().tolist()],
             )
             if setting != "pancancer":
-                X_test = pd.DataFrame(
-                    ct.transform(X_test), columns=X_train.columns
-                )
+                X_test = pd.DataFrame(ct.transform(X_test), columns=X_train.columns)
             else:
                 for cancer in config["tcga_cancers"]:
-                    X_test[cancer] = pd.DataFrame(
-                        ct.transform(X_test[cancer]), columns=X_train.columns
-                    )
+                    X_test[cancer] = pd.DataFrame(ct.transform(X_test[cancer]), columns=X_train.columns)
             base_net_params = {
                 "module__params": params,
                 "optimizer": torch.optim.SGD,
@@ -340,14 +276,10 @@ def main(
                 "optimizer__nesterov": True,
                 "max_epochs": params.get("max_epochs"),
                 "lr": params.get("initial_lr"),
-                "train_split": StratifiedSkorchSurvivalSplit(
-                    params.get("valid_split_size"), stratified=True
-                ),
+                "train_split": StratifiedSkorchSurvivalSplit(params.get("valid_split_size"), stratified=True),
                 "batch_size": params.get("batch_size"),
                 "module__blocks": get_blocks(X_train.columns),
-                "module__p_multimodal_dropout": params.get(
-                    "p_multimodal_dropout"
-                ),
+                "module__p_multimodal_dropout": params.get("p_multimodal_dropout"),
                 "module__missing_modalities": missing_modalities,
                 "callbacks": [
                     (
@@ -368,9 +300,7 @@ def main(
                     ),
                     (
                         "clip",
-                        GradientNormClipping(
-                            gradient_clip_value=params.get("clip", 1)
-                        ),
+                        GradientNormClipping(gradient_clip_value=params.get("clip", 1)),
                     ),
                 ],
             }
@@ -391,23 +321,17 @@ def main(
                     module__noise_factor=params.get("noise_factor"),
                 )
             else:
-                raise ValueError(
-                    "Model name must be in ['poe', 'dae', 'mean']"
-                )
+                raise ValueError("Model name must be in ['poe', 'dae', 'mean']")
 
             net.set_params(**base_net_params)
 
             # using if-else for net is the best and efficient option
             param_distributions = {
                 "module__alpha": loguniform(alpha_range[0], alpha_range[1]),
-                "module__p_dropout": loguniform(
-                    p_dropout_range[0], p_dropout_range[1]
-                ),
+                "module__p_dropout": loguniform(p_dropout_range[0], p_dropout_range[1]),
             }
             if model_name == "poe":
-                param_distributions.update(
-                    {"module__beta": loguniform(beta_range[0], beta_range[1])}
-                )
+                param_distributions.update({"module__beta": loguniform(beta_range[0], beta_range[1])})
             grid = RandomizedSearchCV(
                 net,
                 param_distributions,
@@ -422,26 +346,15 @@ def main(
                 error_score="raise",
                 cv=StratifiedSurvivalKFold(n_splits=5),
             )
-            grid.fit(
-                X_train.to_numpy().astype(np.float32), y_train.astype(str)
-            )
+            grid.fit(X_train.to_numpy().astype(np.float32), y_train.astype(str))
             logger.info("Network Fitting Done")
             if setting != "pancancer":
-                survival_functions = (
-                    grid.best_estimator_.predict_survival_function(
-                        X_test.to_numpy().astype(np.float32)
-                    )
+                survival_functions = grid.best_estimator_.predict_survival_function(
+                    X_test.to_numpy().astype(np.float32)
                 )
                 survival_probabilities = np.stack(
                     [
-                        i(
-                            pd.Series(y_train)
-                            .str.rsplit("|")
-                            .apply(lambda x: int(x[0]))
-                            .values
-                        )
-                        .detach()
-                        .numpy()
+                        i(pd.Series(y_train).str.rsplit("|").apply(lambda x: int(x[0])).values).detach().numpy()
                         for i in survival_functions
                     ]
                 )
@@ -449,10 +362,7 @@ def main(
 
                 sf_df = pd.DataFrame(
                     survival_probabilities,
-                    columns=pd.Series(y_train)
-                    .str.rsplit("|")
-                    .apply(lambda x: int(x[0]))
-                    .values,
+                    columns=pd.Series(y_train).str.rsplit("|").apply(lambda x: int(x[0])).values,
                 )
 
                 sf_df.to_csv(
@@ -465,16 +375,11 @@ def main(
                 logger.info("Saving models and loss")
             else:
                 for ix, cancer in enumerate(config["tcga_cancers"]):
-                    survival_functions = (
-                        grid.best_estimator_.predict_survival_function(
-                            X_test[cancer].to_numpy().astype(np.float32)
-                        )
+                    survival_functions = grid.best_estimator_.predict_survival_function(
+                        X_test[cancer].to_numpy().astype(np.float32)
                     )
                     survival_probabilities = np.stack(
-                        [
-                            i(time[train_splits[ix]]).detach().numpy()
-                            for i in survival_functions
-                        ]
+                        [i(time[train_splits[ix]]).detach().numpy() for i in survival_functions]
                     )
                     logger.info("Converting surv prob to df and saving")
 
