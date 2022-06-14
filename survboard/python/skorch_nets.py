57e07c0e09fb2eb42519f1e0d806c9d359f63a55
slightlier <slightlier@gmail.com>
2022-06-14 17:45:02 +0200

    [CHORE] Dump skorch nets

--- survboard/python/skorch_nets.py
+++ survboard/python/skorch_nets.py
@@ -2,8 +2,8 @@ import torch
 from skorch.net import NeuralNet
 from skorch.utils import to_tensor
 
-from survboard.python.breslow import BreslowEstimator
-from survboard.python.utils.utils import (
+from survival_benchmark.python.breslow import BreslowEstimator
+from survival_benchmark.python.utils.utils import (
     inverse_transform_survival_target,
 )
 
@@ -152,7 +152,7 @@ class DAENet(CoxPHNet):
         time = to_tensor(time, device=self.device)
         event = to_tensor(event, device=self.device)
         y_true = torch.stack([time, event], axis=1)
-        return self.criterion_(y_pred, y_true, self.module_.alpha)
+        return self.criterion_(y_pred, y_true)
 
     def fit(self, X, y=None, **fit_params):
         if not self.warm_start or not self.initialized_:
@@ -163,81 +163,8 @@ class DAENet(CoxPHNet):
         self.train_event = event
         self.partial_fit(X, y, **fit_params)
         self.fit_breslow(
-            self.module_.forward(torch.tensor(X))[0].detach().numpy().ravel(),
+            self.module_.forward(torch.tensor(X)).detach().numpy().ravel(),
             time,
             event,
         )
         return self
-
-    def forward(
-        self,
-        X,
-        training=False,
-        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-    ):
-        y_infer = list(self.forward_iter(X, training=training, device=device))
-        if len(y_infer) > 1:
-            y_infer = torch.cat([i[0] for i in y_infer], axis=0)
-        else:
-            y_infer = y_infer[0][0]
-        return y_infer
-
-
-class IntermediateFusionPoENet(CoxPHNet):
-    def get_loss(self, y_pred, y_true, X=None, training=False):
-        """Return the loss for this batch.
-        Parameters
-        ----------
-        y_pred : torch tensor
-          Predicted target values
-        y_true : torch tensor
-          True target values.
-        X : input data, compatible with skorch.dataset.Dataset
-          By default, you should be able to pass:
-            * numpy arrays
-            * torch tensors
-            * pandas DataFrame or Series
-            * scipy sparse CSR matrices
-            * a dictionary of the former three
-            * a list/tuple of the former three
-            * a Dataset
-          If this doesn't work with your data, you have to pass a
-          ``Dataset`` that can deal with the data.
-        training : bool (default=False)
-          Whether train mode should be used or not.
-        """
-        time, event = inverse_transform_survival_target(y_true)
-        time = to_tensor(time, device=self.device)
-        event = to_tensor(event, device=self.device)
-        y_true = torch.stack([time, event], axis=1)
-        return self.criterion_(
-            y_pred, y_true, self.module_.alpha, self.module_.beta
-        )
-
-    def fit(self, X, y=None, **fit_params):
-        if not self.warm_start or not self.initialized_:
-            self.initialize()
-
-        time, event = inverse_transform_survival_target(y)
-        self.train_time = time
-        self.train_event = event
-        self.partial_fit(X, y, **fit_params)
-        self.fit_breslow(
-            self.module_.forward(torch.tensor(X))[0].detach().numpy().ravel(),
-            time,
-            event,
-        )
-        return self
-
-    def forward(
-        self,
-        X,
-        training=False,
-        device=torch.device("cuda" if torch.cuda.is_available() else "cpu"),
-    ):
-        y_infer = list(self.forward_iter(X, training=training, device=device))
-        if len(y_infer) > 1:
-            y_infer = torch.cat([i[0] for i in y_infer], axis=0)
-        else:
-            y_infer = y_infer[0][0]
-        return y_infer
