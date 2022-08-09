d5404494924274b6869842e8baadbeb5105f94be
slightlier <slightlier@gmail.com>
2022-08-09 11:55:18 +0200

    [CHORE] Clean up examples folder

--- examples/scripts/example_missing_setting.R
+++ examples/scripts/example_missing_setting.R
@@ -135,3 +135,21 @@ run_missing_setting_example_R <- function(project, cancer, n_cores) {
     }
   }
 }
+
+suppressPackageStartupMessages({
+  library(here)
+  library(argparse)
+})
+
+
+parser <- ArgumentParser()
+parser$add_argument("--project", help = "Which project is being used. Must be one of TCGA, ICGC or TARGET.")
+parser$add_argument("--cancer", help = "Which cancer is being used. Must be one of the valid cancer datasets for TCGA, ICGC, or TARGET. See our webservice or paper for details.")
+parser$add_argument("--n_cores", help = "How many cores you would like to use.")
+args <- parser$parse_args()
+
+run_missing_setting_example_R(
+  project = args$project,
+  cancer = args$cancer,
+  n_cores = args$n_cores
+)
